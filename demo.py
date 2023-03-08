21a96644e4e2d0119272f4028bb8b706119ec030
Rohit-K-Gaikwad <rgaikwad1998@gmail.com>
2023-03-08 13:21:24 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
-"""
-demo
-"""
+num1= 10
+num2= 20
+
+print("Addition of given numbers:",num1+num2)
+"""demo"""
+print("Multiplication of given numbers:",num1*num2)
+print("Division of given numbers:",num1/num2)
+print("Modulo of given numbers:",num1%num2)
