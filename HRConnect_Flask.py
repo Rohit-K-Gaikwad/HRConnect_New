69c5d7b799163f36c625ea22a5991cab51f0fc62
Rohit-K-Gaikwad <rgaikwad1998@gmail.com>
2023-03-16 02:36:07 +0530

    HRConnnect Project Completed with Flask Application

--- HRConnect_Flask.py
+++ HRConnect_Flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from task_one import do_task_one
 from task_two import do_task_two
@@ -9,18 +9,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "<h1> Hello, Welcome to HRConnect Project </h1>"
+    return render_template("welcome.html")
 
 
-@app.route("/one")
+@app.route("/task_one")
 def call_task_one():
-    return do_task_one()
+    return render_template("one.html", emp_data=do_task_one())
 
 
-@app.route("/two")
+@app.route("/task_two")
 def call_task_two():
-    return do_task_two()
+    return render_template("two.html", get_data=do_task_two())
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8000, debug=True)
+    app.run(host="localhost", port=4000, debug=True)
